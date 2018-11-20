87f2fe4540b6d23ee7c6131bac532cffb42ac988
Noah Kittleson <noahkittleson@gmail.com>
2018-11-19 21:31:12 -0800

    messed around with putting state and functions into JS classes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,46 @@ import './App.css';
 
 const element = <p>This is supposed to be here</p>
 
+function test () {
+	return <h1> This is a <i>Header</i> </h1>
+}
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+class AnotherTest extends React.Component {
+	render() {
+		return <h1>Hello {this.props.name}</h1>;
+	}
+}
+
+class Toggle extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {isToggleOn: true };
+		//this is necessary to make this a function that can change state of the class
+		//unbounded functions are unable to modify the state of the class because JS is dumb and member functions
+		//aren't functions that have any connection to the class itself, they more just use it as a namespace.
+		//... or you could put handleClick = () => {/definition/}.  Although this seems more explicit.
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick () {
+		this.setState(state => ({
+			isToggleOn: !state.isToggleOn
+		}));
+	}
+
+	render () {
+		return <button onClick = {this.handleClick}> 
+			{this.state.isToggleOn ? 'YES' : 'NO'}
+		</button>
+	}
+}
+
 class App extends Component {
   render() {
     return (
@@ -20,7 +60,10 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             {element}
+            {test()}
+            <AnotherTest name="Noah" />
           </a>
+          <Toggle />
         </header>
       </div>
     );
